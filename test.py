7c1660fa6da595e63e120e327724c2c64ccfb319
Kourosh Vali <kvali@ucdavis.edu>
2020-04-10 23:02:04 -0700

    pycharm folder aded

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
 # test file
+# go to environment
+# source /Users/kourosh/opt/anaconda3/bin/activate
+# conda activate stat
+
+# install conda packages ipython, jupyter, pickle, numpy, scipy, matplotlib
+# install pip packages pickle
+# pickle?
+
+
 
